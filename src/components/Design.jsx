8bc6a91c6329e9fa6b00f6379f945af4ec0c6e27
Jasmine Leger <Jasmineleger21@gmail.com>
2024-06-19 10:33:42 -0400

    added css to the home page

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import '../../styles/design.css'
+import '../../styles/Design.css'
 
 
 export default function Design() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,31 @@
-
+import '../../styles/Home.css'
 
 
 const Home = () => {
-    return <div className="hero is-fullheight-with-navbar">
-      <div className="hero-body">
+  return (
+    <>
+       <div className="hero is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 id="home-page-header" className="title is-1 has-text-weight-bold animated-bounce-in">
+              Shirtify
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="text-section">
         <div className="container has-text-centered">
           <h1 className="subtitle mb-5">
-            Design your own Tee! 
+            Your Imagination, Your Canvas
           </h1>
-          <h2 className="title" id="home-page-header">
-            👚Your Imagination, Your Canvas 🎨
+          <h2 className="title">
+            Custom Tee Creations
           </h2>
         </div>
       </div>
-    </div>
-  }
-  
-  export default Home
+    </>
+  );
+}
+
+
+export default Home
