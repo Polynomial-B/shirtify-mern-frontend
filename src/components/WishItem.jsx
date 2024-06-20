fc268710f8d1e51e89514a15760f695b71d2cfaa
Matt <mphlamb@gmail.com>
2024-06-20 17:57:02 +0200

    merge conflict changes

--- src/components/WishItem.jsx
+++ src/components/WishItem.jsx
@@ -2,51 +2,50 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate, Link, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import "../../styles/WishItem.css";
 
 
 export default function WishItem() {
-  const { wishId } = useParams();
-  const navigate = useNavigate();
-  const [shirt, setShirt] = useState(null);
-  const [color, setColor] = useState('')
-  const [frontDesign, setFrontDesign] = useState('');
-  const [size, setSize] = useState('')
+    const { wishId } = useParams();
+    const navigate= useNavigate();
+    const [shirt, setShirt]= useState(null);
+    const [color, setColor]= useState('')
+    const [frontDesign, setFrontDesign] =useState('');
+    const [size, setSize] = useState('')
 
-  useEffect(() => {
+    const formColors = [
+      "White",
+      "Black",
+      "Red",
+      "Green",
+      "Grey",
+      "Purple",
+      "Pink",
+      "Blue",
+      "Yellow"
+    ];
+
+useEffect(()=> {
     async function fetchShirt() {
-      try {
-        const token = localStorage.getItem('token');
-        const response = await axios.get(`/api/wishlist/${wishId}`, {
-          headers: { Authorization: `Bearer ${token}` }
-        });
-        setShirt(response.data);
-        setColor(response.data.color);
-        setFrontDesign(response.data.frontDesign);
-        setSize(response.data.size);
-      } catch (err) {
-        toast.error('Error fetching shirt');
-      }
-    }
-    fetchShirt();
-  }, [wishId]);
+        try {
+            const token = localStorage.getItem('token');
+            const response = await axios.get(`/api/wishlist/${wishId}`, {
+              headers: { Authorization: `Bearer ${token}` }
+            });
+            setShirt(response.data);
+            setColor(response.data.color);
+            setFrontDesign(response.data.frontDesign);
+            setSize(response.data.size);
+          } catch (err) {
+            toast.error('Error fetching shirt');
+          }
+        }
+        fetchShirt();
+      }, [wishId]);
 
 
 
-  const handleDelete = async () => {
+const handleDelete = async () => {
     try {
-<<<<<<< HEAD
-      const token = localStorage.getItem('token');
-      await axios.delete(`/api/shirts/${wishId}`, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
-      toast.success('Shirt deleted sucessfully');
-      navigate('/wishlist');
-    } catch {
-      toast.error('Error deleting Shirt');
-    }
-  }
-=======
         const token =localStorage.getItem('token');
         await axios.delete(`/api/wishlist/${wishId}`, {
             headers: { Authorization: `Bearer ${token}` }
@@ -57,12 +56,17 @@ export default function WishItem() {
             toast.error('Error deleting Shirt');
         }
 }
->>>>>>> ecbdf88907db255dcbd884e170dd9419f842f43d
 
-  console.log("Hello");
+const token = localStorage.getItem("token");
 
-  const handleSubmit = async (e) => {
+const user = JSON.parse(window.atob(token.split(".")[1]));
+console.log(user.userId);
+
+
+
+const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
       const token = localStorage.getItem('token');
       await axios.put(`/api/shirts/${wishId}`, { color, frontDesign, size }, {
@@ -75,79 +79,71 @@ export default function WishItem() {
     }
   };
 
+    
+    if(!shirt) {
+        return <p> Choose a shirt or design one!</p>
+    }
 
-  if (!shirt) {
-    return <p> Loading...</p>
-  }
-
-  return (
-    <>
-      <h1>Edit Shirt</h1>
-      <div className="section">
-        <div className="columns">
-          <div className="column is-half">
-            <div className="container t-shirt-container" id={color}>
-              <div className="container image-overlay-container">
-                <img src={frontDesign} />
+    return (
+        <>
+          <h1>Edit Shirt</h1>
+          <form>
+            <div className="field">
+              <label className="label">Color</label>
+              <div className="control">
+                <select
+                  className="input" 
+                  type="text" 
+                  value={color} 
+                  onChange={(e) => setColor(e.target.value)} 
+                >
+                <option value="">Select Color</option>
+                  {formColors.map((color) => (
+                    <option key={color} value={color}>
+                      {color}
+                    </option>
+                  ))}
+                  </select>
               </div>
             </div>
-          </div>
-          <div className="column is-half">
-            <div className="section">
-              <div className="container"></div>
-              <form onSubmit={handleSubmit}>
-                <div className="field">
-                  <label className="label">Color</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      value={color}
-                      onChange={(e) => setColor(e.target.value)}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Front Design</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      value={frontDesign}
-                      onChange={(e) => setFrontDesign(e.target.value)}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Size</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      value={size}
-                      onChange={(e) => setSize(e.target.value)}
-                    />
-                  </div>
-                </div>
-                <div className="field is-grouped">
-                  <div className="control">
-                    <button className="button is-link" type="submit">Save</button>
-                  </div>
-                  <div className="control">
-                    <button
-                      className="button is-danger"
-                      type="button"
-                      onClick={handleDelete}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </div>
-              </form>
+            <div className="field">
+              <label className="label">Front Design</label>
+              <div className="control">
+                <input 
+                  className="input" 
+                  type="text" 
+                  value={frontDesign} 
+                  onChange={(e) => setFrontDesign(e.target.value)} 
+                />
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+            <div className="field">
+              <label className="label">Size</label>
+              <div className="control">
+                <input 
+                  className="input" 
+                  type="text" 
+                  value={size} 
+                  onChange={(e) => setSize(e.target.value)} 
+                />
+              </div>
+            </div>
+            <div className="field is-grouped">
+              <div className="control">
+                <button onClick={handleSubmit} className="button is-link" type="submit">Save</button>
+              </div>
+              <div className="control">
+                <button 
+                  className="button is-danger" 
+                  type="button" 
+                  onClick={handleDelete}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </form>
+        </>
+      );
+    }
+    
