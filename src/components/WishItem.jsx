e7ae83b3371a9efa098c3006f03738384b6ff619
Matt <mphlamb@gmail.com>
2024-06-20 18:01:32 +0200

    merge conflict fix

--- src/components/WishItem.jsx
+++ src/components/WishItem.jsx
@@ -85,65 +85,74 @@ const handleSubmit = async (e) => {
     }
 
     return (
-        <>
-          <h1>Edit Shirt</h1>
-          <form>
-            <div className="field">
-              <label className="label">Color</label>
-              <div className="control">
-                <select
-                  className="input" 
-                  type="text" 
-                  value={color} 
-                  onChange={(e) => setColor(e.target.value)} 
-                >
-                <option value="">Select Color</option>
-                  {formColors.map((color) => (
-                    <option key={color} value={color}>
-                      {color}
-                    </option>
-                  ))}
-                  </select>
+      <>
+        <h1>Edit Shirt</h1>
+        <div className="section">
+          <div className="columns">
+            <div className="column is-half">
+              <div className="container t-shirt-container" id={color}>
+                <div className="container image-overlay-container">
+                  <img src={frontDesign} />
+                </div>
               </div>
             </div>
-            <div className="field">
-              <label className="label">Front Design</label>
-              <div className="control">
-                <input 
-                  className="input" 
-                  type="text" 
-                  value={frontDesign} 
-                  onChange={(e) => setFrontDesign(e.target.value)} 
-                />
+            <div className="column is-half">
+              <div className="section">
+                <div className="container"></div>
+                <form onSubmit={handleSubmit}>
+                  <div className="field">
+                    <label className="label">Color</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        value={color}
+                        onChange={(e) => setColor(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Front Design</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        value={frontDesign}
+                        onChange={(e) => setFrontDesign(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label className="label">Size</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        value={size}
+                        onChange={(e) => setSize(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="field is-grouped">
+                    <div className="control">
+                      <button className="button is-link" type="submit">Save</button>
+                    </div>
+                    <div className="control">
+                      <button
+                        className="button is-danger"
+                        type="button"
+                        onClick={handleDelete}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-            <div className="field">
-              <label className="label">Size</label>
-              <div className="control">
-                <input 
-                  className="input" 
-                  type="text" 
-                  value={size} 
-                  onChange={(e) => setSize(e.target.value)} 
-                />
-              </div>
-            </div>
-            <div className="field is-grouped">
-              <div className="control">
-                <button onClick={handleSubmit} className="button is-link" type="submit">Save</button>
-              </div>
-              <div className="control">
-                <button 
-                  className="button is-danger" 
-                  type="button" 
-                  onClick={handleDelete}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </form>
-        </>
-      );
-    }
+          </div>
+        </div>
+      </>
+    );
+  }
     
